d62a85c7b20423cbbabfe64734f02e277b6da4ed
kula <mika.legall@live.fi>
2022-11-04 11:06:01 +0200

    Part 5.22 & Remove view status from DB

--- part5/bloglist-backend/controllers/blogs.js
+++ part5/bloglist-backend/controllers/blogs.js
@@ -65,40 +65,39 @@ blogsRouter.get('/:id', async (request, response) => {
 // REST crUd: UPDATE
 blogsRouter.put('/:id', async (request, response) => {
   // https://github.com/blakehaswell/mongoose-unique-validator#find--updates
-  const { title, author, url, likes, viewAll } = request.body
+  const { title, author, url, likes } = request.body
 
-const userId = request.headers.user._id.toString()
+  const userId = request.headers.user._id.toString()
 
-const blog = await Blog.findById(request.params.id)
+  const blog = await Blog.findById(request.params.id)
 
-if (!blog) {
-  return response.status(400).json({
-    error: `Id ${request.params.id} does not exist`
-  })
-} else if (blog.user.toString() === userId) {
+  if (!blog) {
+    return response.status(400).json({
+      error: `Id ${request.params.id} does not exist`
+    })
+  } else if (blog.user.toString() === userId) {
 
-  const newBlogEntry = {
-    title: title,
-    author: author,
-    url: url,
-    likes: likes || 0,
-    //viewAll: viewAll,
-    user: userId
+    const newBlogEntry = {
+      title: title,
+      author: author,
+      url: url,
+      likes: likes || 0,
+      user: userId
+    }
+
+    // https://fullstackopen.com/en/part3/saving_data_to_mongo_db#other-operations
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlogEntry, { new: true, runValidators: true, context: 'query' })
+    //user.blogs = user.blogs.concat(updatedBlog._id)
+    //await user.save()
+    response.json(updatedBlog)
+
+  } else {
+
+    return response.status(403).json({
+      error: 'Blog can be updated only by the owner'
+    })
+
   }
-
-  // https://fullstackopen.com/en/part3/saving_data_to_mongo_db#other-operations
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlogEntry, { new: true, runValidators: true, context: 'query' })
-  //user.blogs = user.blogs.concat(updatedBlog._id)
-  //await user.save()
-  response.json(updatedBlog)
-
-} else {
-
-  return response.status(403).json({
-    error: 'Blog can be updated only by the owner'
-  })
-
-}
 
 
 })
